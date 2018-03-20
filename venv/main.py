1d7dcde7e5f2b52a420e7fd780256d3a36847bec
shivampahwa9 <shivampahwa.9@gmail.com>
2018-03-20 22:38:03 +0530

    user's name

---
+++ venv/main.py
@@ -0,0 +1,13 @@
+print("Spychat")
+
+spy_salutation=input("What should we call you(Mr. or Ms.)?")
+
+print(spy_salutation)
+
+spy_name=input("What is your name?")
+
+print(spy_name)
+
+print("Welcome to spychat" +" "+ spy_salutation +" "+ spy_name)
+
+print("Alright"+" "+ spy_salutation +spy_name+" " +"I'd like to know a little bit more about you....")
