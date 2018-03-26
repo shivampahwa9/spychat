f04ddbb73990d764372060365d13c8197b079522
shivampahwa9 <shivampahwa.9@gmail.com>
2018-03-26 20:20:04 +0530

    first module completed

--- venv/main.py
+++ venv/main.py
@@ -1,3 +1,4 @@
+import spy_details
 print("Spychat")
 
 spy_salutation=input("What should we call you(Mr. or Ms.)?")
@@ -10,11 +11,11 @@ print(spy_name)
 
 print("Welcome to spychat" +" "+ spy_salutation +" "+ spy_name)
 
-print("Alright"+" "+ spy_salutation +spy_name+" " +"I'd like to know a little bit more about you....")
+print("Alright"+" "+ spy_salutation +" "+spy_name+" " +"I'd like to know a little bit more about you....")
 ####### new user or default ####
 user=input("Do you want to create as a new user(YES) or continue as a default user (NO)")
 print(user)
-if len(user)< 3:
+if len(user)>=3:
     spy_name1=input("what is your name?")
     print(spy_name1)
     spy_salutation1=input("what should we call you(Mr. or Ms.)?")
@@ -31,4 +32,9 @@ if len(user)< 3:
         print("not good")
     print("welcome to spychat" +" "+spy_salutation1+" "+spy_name1+".You are"+" "+str(spy_age)+" "+"years old."+" "+str(spy_rating)+" "+"rating you have given")
 else:
-    print("continue as a default ")
+    print("continue as a "+spy_details.spy_salutation+" "+spy_details.spy_name)
+    print(spy_details.spy_salutation+" "+spy_details.spy_name)
+    print("Age"+str(spy_details.spy_age))
+
+
+
---
+++ venv/spy_details.py
@@ -0,0 +1,4 @@
+spy_name="shivam"
+spy_salutation="Mr"
+spy_age=21
+spy_rating=5
