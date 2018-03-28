d644b4e4aa86aacd9ee73425139f79e0835d3a5f
shivampahwa9 <shivampahwa.9@gmail.com>
2018-03-28 19:57:20 +0530

    add a status

--- venv/main.py
+++ venv/main.py
@@ -1,3 +1,4 @@
+from spy_details import spy_name,spy_salutation,spy_age
 
 #print("Spychat")
 
@@ -18,27 +19,27 @@ STATUS_MESSAGES=["MY NAME IS SHIVAM","SHAKEN,NOT STIRRED"]
 def add_status(current_status):
 
     if current_status!= None:
-        print("your current status is"+current_status+"\n")
+        print("your current status is "+ current_status+"\n")
     else:
         print("you do not have any current status")
-    default = input("Do you want to select from the older status (y/n)? ")
+    default = input("\nDo you want to select from the older status (y/n)? ")
     if default.upper() == "N":
-        new_status_message = input("What status message do you want to set?")
+        new_status_message = input("\nWhat status message do you want to set?")
 
         if len(new_status_message) > 0:
             updated_status_message = new_status_message
             STATUS_MESSAGES.append(updated_status_message)
             print(STATUS_MESSAGES)
 
-        elif default.upper() == "Y":
-            item_position = 1
-            for message in STATUS_MESSAGES:
-                print (str(item_position) +"."+message)
-                item_position = item_position+1
-            message_selection = int(input("\nchoose from the above messages"))
-            if len(STATUS_MESSAGES) >= message_selection:
-                updated_status_message = STATUS_MESSAGES[message_selection-1]
-        return updated_status_message
+    elif default.upper() == "Y":
+        item_position = 1
+        for message in STATUS_MESSAGES:
+            print (str(item_position) +"."+message)
+            item_position = item_position+1
+        message_selection = int(input("\nchoose from the above messages"))
+        if len(STATUS_MESSAGES) >= message_selection:
+            updated_status_message = STATUS_MESSAGES[message_selection-1]
+    return updated_status_message
 
 
 
@@ -51,14 +52,11 @@ def start_chat(spy_name,spy_age):
 
         if menu_choice == 1:
             print("update the status what you want to update")
-            add_status(current_status)
+            current_status=add_status(current_status)
         elif menu_choice== 2 :
             show_menu=False
 
 
-from spy_details import spy_name
-from spy_details import spy_salutation
-from spy_details import spy_age
 
 user=("continue as a "+spy_salutation+" "+spy_name+" (Y/N)")
 existing=input(user)
